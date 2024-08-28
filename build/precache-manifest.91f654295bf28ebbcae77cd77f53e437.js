183006f9655179e13d10490207aeb2c592821e57
ARPIT <agrawalarpit414@gmail.com>
2024-08-29 01:44:01 +0530

    update the vercel buld

---
+++ build/precache-manifest.91f654295bf28ebbcae77cd77f53e437.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe0f964d930fa2be41d5c7aa07c7bb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aed8ede08c62f89a393d",
+    "url": "/static/css/2.903596ff.chunk.css"
+  },
+  {
+    "revision": "f6962b97c00ab231784d",
+    "url": "/static/css/main.389d2c7f.chunk.css"
+  },
+  {
+    "revision": "aed8ede08c62f89a393d",
+    "url": "/static/js/2.b341da33.chunk.js"
+  },
+  {
+    "revision": "d31f46e27f9ac8a0348758e0587d93e0",
+    "url": "/static/js/2.b341da33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dbd0bb3f55ee84de8c6",
+    "url": "/static/js/3.c55d2aaa.chunk.js"
+  },
+  {
+    "revision": "f6962b97c00ab231784d",
+    "url": "/static/js/main.cbd91599.chunk.js"
+  },
+  {
+    "revision": "0a18a5e1eaa6206c8221",
+    "url": "/static/js/runtime-main.c6a2ca93.js"
+  }
+]);
